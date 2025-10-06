483755e5d8fd2ffa85b868092c74ac4b9d5c0e06
Anton Fruhmann <toni.fruhmann@googlemail.com>
2025-10-06 22:46:29 +0200

    build: disable ESLint during production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'community.cloudflare.steamstatic.com' },
+    ],
+  },
 };
 
 export default nextConfig;
